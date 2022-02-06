2fd816577dd06e6eab40347d5eb5590c48c2d0bd
abdulkshahid <abdulkshahid@gmail.com>
2022-02-07 00:37:59 +0100

    added prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   firebase: {
-    projectId: 'ticketing-fe06a',
-    appId: '1:439023358374:web:f2dffcb74590885800bf67',
-    storageBucket: 'ticketing-fe06a.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyCJia7AgEFsQ-gmu9rqlDBL9uGBPxwsSUI',
-    authDomain: 'ticketing-fe06a.firebaseapp.com',
-    messagingSenderId: '439023358374',
-    measurementId: 'G-JGG5GLJCVH',
+    apiKey: "AIzaSyCP3XLQzxSQaqYv55fNT6AMrRoMiulHlU0",
+    authDomain: "ticketing-fe06a-prod.firebaseapp.com",
+    projectId: "ticketing-fe06a-prod",
+    storageBucket: "ticketing-fe06a-prod.appspot.com",
+    messagingSenderId: "997910614767",
+    appId: "1:997910614767:web:be499d72ec72de2449c11a",
+    measurementId: "G-Q9GNB2VY83"
   },
   production: true
 };
+
+//check permissions on cloud firestore
